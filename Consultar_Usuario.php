0aaf8808308fadbcb893385cb639a74760c0b240
Cecili-Rojas <67488180+Cecili-Rojas@users.noreply.github.com>
2020-10-05 18:39:45 -0500

    Add files via upload

--- Consultar_Usuario.php
+++ Consultar_Usuario.php
@@ -1,27 +1,41 @@
 <?PHP
-$ hostname_localhost = "mysql-jeanpantoja1.alwaysdata.net" ;
-$ database_localhost = "jeanpantoja1_smartcitybd" ;
-$ username_localhost = "215238_root" ;
-$ password_localhost = "smartcity123" ;
+$hostname_localhost ="mysql-jeanpantoja1.alwaysdata.net";
+$database_localhost ="jeanpantoja1_smartcitybd";
+$username_localhost = "215238_root";
+$password_localhost = "smartcity123";
 
-$ json=array();
+$json=array();
 
-    if(isset($ _GET["ID_Usuario"])){
+    if(isset($_GET["ID_Usuario"])){
 
-            $ ID_Usuario=$ _GET['ID_Usuario'];
+            $ID_Usuario=$_GET['ID_Usuario'];
 
 
-            $ conexion=mysqli_connect($ hostname_localhost,$ username_localhost,$ password_localhost,$ database_localhost);
+            $conexion=mysqli_connect($hostname_localhost,$username_localhost,$password_localhost,$database_localhost);
 
-            $ consulta="select ID_Usuario, US_Nombres, US_A funciones,US_Direcccion,US_Fecha_Nacimiento,US_Nacionalidad,US_Telefono,US_Email,US_Contraseña,US_Tipo from Tbl_Usuario where ID_Usuario= '{$ID_Usuario}'";
+            $consulta="select ID_Usuario, US_Nombres, US_A funciones,US_Direcccion,US_Fecha_Nacimiento,US_Nacionalidad,US_Telefono,US_Email,US_Contraseña,US_Tipo from Tbl_Usuario where ID_Usuario= '{$ID_Usuario}'";
 
-            $ resultado=mysqli_query($ conexion,$ consulta);
+            $resultado=mysqli_query($conexion,$consulta);
 
-            if($ registro=mysqli_fetch_array($ resultado)){
-                $ json['usuario'][]=$ registro;
+            if($registro=mysqli_fetch_array($resultado)){
+                $json['usuario'][]=$registro;
             }
 
             else{
+                $resultar["ID_Usuario"]=0;
+                $resultar["US_Nombres"]='No registra';
+                $resultar["US_A funciones"]='No registra';
+                 $resultar["US_Direcccion"]='No registra';
+                $resultar["US_Fecha_Nacimiento"]='No registra';
+                 $resultar["US_Nacionalidad"]='No registra';
+                $resultar["US_Telefono"]='No registra';
+                 $resultar["US_Email"]='No registra';
+                $resultar["US_Contraseña"]='No registra';
+                $resultar["US_Tipo"]='No registra';
+                $json['usuario'][]=$resultar;
+
+
+
                 $ resultar [ "ID_Usuario" ] = "Sin usuario" ;
             $ resultar [ 'US_Nombres' ] = "Sin usuario" ;
             $ resultar [ 'US_A funciones' ] = "Sin usuario" ;
@@ -34,20 +48,17 @@ $ json=array();
             $ resultar [ 'US_Tipo' ] = "Sin usuario" ;
             $ json [ 'usuario' ] [] = $ resultar ;
 
-
-
-
             }
 
-            mysqli_close($ conexion);
-            echo json_encode($ json);
+            mysqli_close($conexion);
+            echo json_encode($json);
     }
 
     else{
-        $ resultar["success"]=0;
-        $ resultar["message"]='WS no retorna';
-        $ json['usuario'][]=$ resultar;
-        echo json_encode($ json);
+        $resultar["success"]=0;
+        $resultar["message"]='WS no retorna';
+        $json['usuario'][]=$resultar;
+        echo json_encode($json);
     }
 
 
