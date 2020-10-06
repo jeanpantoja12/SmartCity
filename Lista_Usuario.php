ece7950f4310f4f698646a304901b2a11bf80eb4
Cecili-Rojas <67488180+Cecili-Rojas@users.noreply.github.com>
2020-10-05 19:56:39 -0500

    Add files via upload

---
+++ Lista_Usuario.php
@@ -0,0 +1,22 @@
+<?php
+$hostname_localhost = "mysql-jeanpantoja1.alwaysdata.net";
+$database_localhost = "jeanpantoja1_smartcitybd";
+$username_localhost = "215238_root";
+$password_localhost = "smartcity123";
+
+$json=array();
+
+
+        $conexion=mysqli_connect($hostname_localhost,$username_localhost,$password_localhost,$database_localhost);
+
+        $consulta="SELECT * FROM `Tbl_Usuario`";
+
+        $resultado=mysqli_query($conexion,$consulta);
+
+        while($registro=mysqli_fetch_array($resultado)){
+            $json['Tbl_Usuario'][]=$registro;
+        }
+
+         mysqli_close($conexion);
+         echo json_encode($json);
+?>
