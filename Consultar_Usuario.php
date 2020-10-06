c9ba29943fb9ea6608ca65f1e778c833cb8147dc
Jean Pantoja <48846207+jeanpantoja12@users.noreply.github.com>
2020-10-05 20:15:47 -0500

    Update Consultar_Usuario.php

--- Consultar_Usuario.php
+++ Consultar_Usuario.php
@@ -13,7 +13,7 @@ $json=array();
 
             $conexion=mysqli_connect($hostname_localhost,$username_localhost,$password_localhost,$database_localhost);
 
-            $consulta="SELECT `ID_Usuario`, `US_Nombres`, `US_Apellidos`, `US_Direcccion`, `US_Fecha_Nacimiento`, `US_Nacionalidad`, `US_Telefono`, `US_Email`, `US_Contrasena`, `US_Tipo` FROM `Tbl_Usuario` WHERE `ID_Usuario`= `{$'ID'}`";
+            $consulta="SELECT `ID_Usuario`, `US_Nombres`, `US_Apellidos`, `US_Direcccion`, `US_Fecha_Nacimiento`, `US_Nacionalidad`, `US_Telefono`, `US_Email`, `US_Contrasena`, `US_Tipo` FROM `Tbl_Usuario` WHERE `ID_Usuario`= {$'ID'}";
 
 
             $resultado=mysqli_query($conexion,$consulta);
