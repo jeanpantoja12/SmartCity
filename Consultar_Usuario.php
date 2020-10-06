e204aa85d0f5d99331ba5489c57ea53708049696
Cecili-Rojas <67488180+Cecili-Rojas@users.noreply.github.com>
2020-10-05 19:51:24 -0500

    Add files via upload

--- Consultar_Usuario.php
+++ Consultar_Usuario.php
@@ -13,7 +13,10 @@ $json=array();
 
             $conexion=mysqli_connect($hostname_localhost,$username_localhost,$password_localhost,$database_localhost);
 
-            $consulta="select ID_Usuario, US_Nombres, US_A funciones,US_Direcccion,US_Fecha_Nacimiento,US_Nacionalidad,US_Telefono,US_Email,US_Contraseña,US_Tipo from Tbl_Usuario where ID_Usuario= '{$ID_Usuario}'";
+            $consulta="SELECT `ID_Usuario`, `US_Nombres`, `US_Apellidos`, `US_Direcccion`, `US_Fecha_Nacimiento`, `US_Nacionalidad`, `US_Telefono`, `US_Email`, `US_Contrasena`, `US_Tipo` FROM `Tbl_Usuario` where `ID_Usuario`= '{$`ID_Usuario`}'";
+
+
+
 
             $resultado=mysqli_query($conexion,$consulta);
 
