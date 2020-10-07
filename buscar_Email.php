e56924739fc84ff1784e73f5e5701622207d7284
Cecili-Rojas <67488180+Cecili-Rojas@users.noreply.github.com>
2020-10-06 20:25:30 -0500

    Add files via upload

---
+++ buscar_Email.php
@@ -0,0 +1,52 @@
+<?PHP
+$hostname_localhost ="mysql-jeanpantoja1.alwaysdata.net";
+$database_localhost ="jeanpantoja1_smartcitybd";
+$username_localhost ="215238_root";
+$password_localhost ="smartcity123";
+
+$json=array();
+
+    if(isset($_GET["US_Email"])){
+
+            $em=$_GET['US_Email'];
+
+
+            $conexion=mysqli_connect($hostname_localhost,$username_localhost,$password_localhost,$database_localhost);
+
+            $consulta="SELECT `ID_Usuario`, `US_Nombres`, `US_Apellidos`, `US_Direcccion`, `US_Fecha_Nacimiento`, `US_Nacionalidad`, `US_Telefono`, `US_Email`, `US_Contrasena`, `US_Tipo` FROM `Tbl_Usuario` WHERE `US_Email`= '{$em}'";
+
+
+            $resultado=mysqli_query($conexion,$consulta);
+
+            if($registro=mysqli_fetch_assoc($resultado)){
+                $json['usuario'][]=$registro;
+            }
+
+            else{
+            $resultar["ID_Usuario"]="No Registrado";
+            $resultar['US_Nombres']="No User";
+            $resultar['US_Apellidos']="No User";
+            $resultar['US_Direccion']="No User";
+            $resultar['US_Fecha_Nacimiento']="No User";
+            $resultar['US_Nacionalidad']="No User";
+            $resultar['US_Telefono']="No User";
+            $resultar['US_Email']="No User";
+            $resultar['US_Contrasena']="No User";
+            $resultar['US_Tipo']="No User";
+                $json['usuario'][]=$resultar;
+
+
+            }
+            mysqli_close($conexion);
+            echo json_encode($json);
+    }
+
+    else{
+        $resultar["success"]=0;
+        $resultar["message"]='WS no retorna';
+        $json['usuario'][]=$resultar;
+        echo json_encode($json);
+    }
+
+
+?>
