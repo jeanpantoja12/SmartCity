7589d5a944a1e7f47ec45c7fec1d38f8de1f91b4
Jean Pantoja <48846207+jeanpantoja12@users.noreply.github.com>
2020-10-07 19:53:50 -0500

    Add files via upload

--- Usuario/login.php
+++ Usuario/login.php
@@ -20,30 +20,39 @@ $db = $database->getConnection();
 $user = new Usuario($db);
 
 // set ID property of user to be edited
-$user->US_Email = isset($_GET['US_Email']) ? $_GET['US_Email'] : die();
-$user->US_Contrasena = isset($_GET['US_Contrasena']) ? $_GET['US_Contrasena'] : die();  
-
-// read the details of user to be edited
-$stmt = $user->login();
-if($stmt->rowCount() > 0){
-    // get retrieved row
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
-    // create array
-    $user_arr=array(
-        "status" => true,
-        "message" => "Successfully Login!",
-        "ID_Usuario" => $row['ID_Usuario'],
-        "US_Email" => $row['US_Email']
-    );
-    http_response_code(200);
-    echo json_encode($user_arr);
+if(isset($_GET['US_Email']) && isset($_GET['US_Contrasena'])){
+    $user->US_Email = isset($_GET['US_Email']) ? $_GET['US_Email'] : die();
+    $user->US_Contrasena = isset($_GET['US_Contrasena']) ? $_GET['US_Contrasena'] : die();
+    $stmt = $user->login();
+    if($stmt->rowCount() > 0){
+        // get retrieved row
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        // create array
+        $user_arr=array(
+            "status" => true,
+            "message" => "Login Satisfactorio!",
+            "ID_Usuario" => $row['ID_Usuario'],
+            "US_Email" => $row['US_Email']
+        );
+        http_response_code(200);
+    }
+    else{
+        $user_arr=array(
+            "status" => false,
+            "message" => "Usuario Incorrecto",
+        );
+        http_response_code(404);
+        
+    }
 }
 else{
     $user_arr=array(
-        "status" => false,
-        "message" => "Invalid Username or Password!",
-    );
-    http_response_code(404);
-    echo json_encode($user_arr);
+            "status" => false,
+            "message" => "Datos Incompletos",
+        );
+    http_response_code(500);
 }
+echo json_encode($user_arr);
+// read the details of user to be edited
+
 ?>
