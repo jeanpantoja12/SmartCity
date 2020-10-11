312577e7341aa88937fa78549f6a4b87338f1360
Cecili-Rojas <67488180+Cecili-Rojas@users.noreply.github.com>
2020-10-11 18:24:32 -0500

    Add files via upload

--- objects/Vehiculo.php
+++ objects/Vehiculo.php
@@ -4,17 +4,17 @@ class Vehiculo{
     // database connection and table name
     private $conn;
     private $table_name = "Tbl_Vehiculo";
-     private $table_name = "Tbl_Conductor";
+    //private $table_name = "Tbl_Conductor";
+    //private $table_name = "Tbl_Tipo_Vehiculo";
   
     // object properties
     public $ID_Vehiculo;
-    public $VEH_Conductor;
     public $VEH_Placa;
     public $VEH_Color;
     public $VEH_Modelo;
     public $VEH_Marca;
-    public $VEH_Tipo;
-    public $ID_Vehiculo;
+    public $ID_Tipo_Vehiculo;
+    public $ID_Conductor;
   ;
 
     // constructor with $db as database connection
@@ -26,27 +26,26 @@ class Vehiculo{
     // query to insert record
     $query = "INSERT INTO
                 " . $this->table_name . "
-            SET VEH_Conductor=:VEH_Conductor, VEH_Placa=:VEH_Placa, VEH_Color=:VEH_Color, VEH_Modelo=:VEH_Modelo, VEH_Marca=:VEH_Marca, VEH_Tipo=:VEH_Tipo, ID_Vehiculo=:ID_Vehiculo";
+            SET VEH_Placa=:VEH_Placa, VEH_Color=:VEH_Color, VEH_Modelo=:VEH_Modelo, VEH_Marca=:VEH_Marca, ID_Tipo_Vehiculo=:ID_Tipo_Vehiculo, ID_Conductor=:ID_Conductor";
 
     // prepare query
     $stmt = $this->conn->prepare($query);
   
     // sanitize
-    $this->VEH_Conductor=htmlspecialchars(strip_tags($this->VEH_Conductor));
     $this->VEH_Placa=htmlspecialchars(strip_tags($this->VEH_Placa));
     $this->VEH_Color=htmlspecialchars(strip_tags($this->VEH_Color));
     $this->VEH_Modelo=htmlspecialchars(strip_tags($this->VEH_Modelo));
     $this->VEH_Marca=htmlspecialchars(strip_tags($this->VEH_Marca));
-    $this->VEH_Tipo=htmlspecialchars(strip_tags($this->VEH_Tipo));
-    $this->ID_Vehiculo=htmlspecialchars(strip_tags($this->ID_Vehiculo));
+    $this->VEH_Tipo=htmlspecialchars(strip_tags($this->ID_Tipo_Vehiculo));
+    $this->ID_Vehiculo=htmlspecialchars(strip_tags($this->ID_Conductor));
     // bind values
-    $stmt->bindParam(":VEH_Conductor", $this->VEH_Conductor);
+
     $stmt->bindParam(":VEH_Placa", $this->VEH_Placa);
     $stmt->bindParam(":VEH_Color", $this->VEH_Color);
     $stmt->bindParam(":VEH_Modelo", $this->VEH_Modelo);
     $stmt->bindParam(":VEH_Marca", $this->VEH_Marca);
-    $stmt->bindParam(":VEH_Tipo", $this->VEH_Tipo);
-    $stmt->bindParam(":ID_Vehiculo", $this->ID_Vehiculo);
+    $stmt->bindParam(":VEH_Tipo", $this->ID_Tipo_Vehiculo);
+    $stmt->bindParam(":ID_Vehiculo", $this->ID_Conductor);
 
     // execute query
     if($stmt->execute()){
