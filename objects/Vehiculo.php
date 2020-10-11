cbba30e1b1a03fe5e0507eac8403a2a887950f35
Cecili-Rojas <67488180+Cecili-Rojas@users.noreply.github.com>
2020-10-11 18:42:38 -0500

    Add files via upload

--- objects/Vehiculo.php
+++ objects/Vehiculo.php
@@ -4,6 +4,8 @@ class Vehiculo{
     // database connection and table name
     private $conn;
     private $table_name = "Tbl_Vehiculo";
+    //private $table_name = "Tbl_Conductor";
+    //private $table_name = "Tbl_Tipo_Vehiculo";
   
     // object properties
     public $ID_Vehiculo;
@@ -13,9 +15,7 @@ class Vehiculo{
     public $VEH_Marca;
     public $ID_Tipo_Vehiculo;
     public $ID_Conductor;
-    public $Nombre_Conductor;
-    public $Tipo_Vehiculo;
-  
+  ;
 
     // constructor with $db as database connection
     public function __construct($db){
@@ -37,14 +37,15 @@ class Vehiculo{
     $this->VEH_Modelo=htmlspecialchars(strip_tags($this->VEH_Modelo));
     $this->VEH_Marca=htmlspecialchars(strip_tags($this->VEH_Marca));
     $this->ID_Tipo_Vehiculo=htmlspecialchars(strip_tags($this->ID_Tipo_Vehiculo));
-    $this->ID_Conductor=htmlspecialchars(strip_tags($this->ID_Conductor));
+    $this->ID_Vehiculo=htmlspecialchars(strip_tags($this->ID_Conductor));
     // bind values
+
     $stmt->bindParam(":VEH_Placa", $this->VEH_Placa);
     $stmt->bindParam(":VEH_Color", $this->VEH_Color);
     $stmt->bindParam(":VEH_Modelo", $this->VEH_Modelo);
     $stmt->bindParam(":VEH_Marca", $this->VEH_Marca);
-    $stmt->bindParam(":ID_Tipo_Vehiculo", $this->ID_Tipo_Vehiculo);
-    $stmt->bindParam(":ID_Conductor", $this->ID_Conductor);
+    $stmt->bindParam(":VEH_Tipo", $this->ID_Tipo_Vehiculo);
+    $stmt->bindParam(":ID_Vehiculo", $this->ID_Conductor);
 
     // execute query
     if($stmt->execute()){
@@ -53,45 +54,6 @@ class Vehiculo{
   
     return false;
       
-    }
-    function Consulta(){
-        // query to read single record
-    $query = "SELECT
-                v.ID_Vehiculo, v.VEH_Placa, v.VEH_Color, v.VEH_Modelo, v.VEH_Marca, v.ID_Tipo_Vehiculo, t.TV_Nombre as Tipo_Vehiculo,v.ID_Conductor,CONCAT(c.CON_Nombre,' ',c.CON_Apellidos) as Nombre_Conductor
-            FROM
-                " . $this->table_name . " v
-                LEFT JOIN
-                    Tbl_Tipo_Vehiculo t
-                        ON v.ID_Tipo_Vehiculo = t.ID_Tipo_Vehiculo
-                LEFT JOIN
-                    Tbl_Conductor c
-                        ON v.ID_Conductor = c.ID_Conductor
-            WHERE
-                v.VEH_Placa = ?
-            LIMIT
-                0,1";
-                // prepare query statement
-    $stmt = $this->conn->prepare( $query );
-  
-    // bind id of product to be updated
-    $stmt->bindParam(1, $this->VEH_Placa);
-  
-    // execute query
-    $stmt->execute();
-  
-    // get retrieved row
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
-  
-    // set values to object properties
-    $this->ID_Vehiculo = $row['ID_Vehiculo'];
-    $this->VEH_Placa = $row['VEH_Placa'];
-    $this->VEH_Color = $row['VEH_Color'];
-    $this->VEH_Modelo = $row['VEH_Modelo'];
-    $this->VEH_Marca = $row['VEH_Marca'];
-    $this->ID_Tipo_Vehiculo = $row['ID_Tipo_Vehiculo'];
-    $this->Tipo_Vehiculo = $row['Tipo_Vehiculo'];
-    $this->ID_Conductor = $row['ID_Conductor'];
-    $this->Nombre_Conductor = $row['Nombre_Conductor'];
-    }
+}
 }
 ?>
