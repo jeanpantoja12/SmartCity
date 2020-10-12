7495f94fcc9915c305b5af447d6922cc9428a964
giancohg <67488267+giancohg@users.noreply.github.com>
2020-10-11 20:50:07 -0500

    Add files via upload

---
+++ Incidente/Consultar_Incidentes.php
@@ -0,0 +1,52 @@
+<?PHP
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Credentials: true");
+header('Content-Type: application/json');
+
+// include database and object files
+include_once '../config/database.php';
+include_once '../objects/Incidente.php';
+
+// get database connection
+$database = new Database();
+$db = $database->getConnection();
+  
+// prepare product object
+$incidente = new Incidente($db);
+// set ID property of record to read
+$incidente->ID_Incidente = isset($_GET['ID_Incidente']) ? $_GET['ID_Incidente'] : die();
+  
+// read the details of product to be edited
+$incidente->Consulta();
+  
+if($incidente->ID_Incidente!=null){
+    // create array
+    $incidente_arr = array(
+        "ID_Vehiculo" =>  $incidente->ID_Vehiculo,
+        "ID_Usuario" => $incidente->ID_Usuario,
+        "ind_Descripcion" => $incidente->ind_Descripcion,
+        "ind_Fotografia" => $incidente->ind_Fotografia,
+        "ind_Fecha_Incidente" => $incidente->ind_Fecha_Incidente,
+        "ID_Incidente" => $incidente->ID_Incidente
+    );
+  
+    // set response code - 200 OK
+    http_response_code(200);
+  
+    // make it json format
+    echo json_encode($incidente_arr);
+}
+  
+else{
+    // set response code - 404 Not found
+    http_response_code(404);
+  
+    // tell the user product does not exist
+    echo json_encode(array("message" => "Incidente no existe."));
+}
+
+
+?>
