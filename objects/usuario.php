272ac837abd295e0bb4718cc30ce09011fa0bb42
Jean Pantoja <48846207+jeanpantoja12@users.noreply.github.com>
2020-10-11 22:12:57 -0500

    Add files via upload

--- objects/usuario.php
+++ objects/usuario.php
@@ -101,5 +101,49 @@ function cambiar_password(){
     }
     return false;
 }
+function update(){
+    // query to insert record
+    $query = "UPDATE
+                " . $this->table_name . "
+            SET 
+            US_Nombres = :US_Nombres,
+            US_Apellidos = :US_Apellidos, 
+            US_Direccion = :US_Direccion,
+            US_Fecha_Nacimiento = :US_Fecha_Nacimiento,
+            US_Nacionalidad = :US_Nacionalidad, 
+            US_Telefono = :US_Telefono,
+            US_Email = :US_Email
+            WHERE
+                ID_Usuario = :ID_Usuario";
+
+    // prepare query
+    $stmt = $this->conn->prepare($query);
+  
+    // sanitize
+    $this->US_Nombres=htmlspecialchars(strip_tags($this->US_Nombres));
+    $this->US_Apellidos=htmlspecialchars(strip_tags($this->US_Apellidos));
+    $this->US_Direccion=htmlspecialchars(strip_tags($this->US_Direccion));
+    $this->US_Fecha_Nacimiento=htmlspecialchars(strip_tags($this->US_Fecha_Nacimiento));
+    $this->US_Nacionalidad=htmlspecialchars(strip_tags($this->US_Nacionalidad));
+    $this->US_Telefono=htmlspecialchars(strip_tags($this->US_Telefono));
+    $this->US_Email=htmlspecialchars(strip_tags($this->US_Email));
+    $this->ID_Usuario=htmlspecialchars(strip_tags($this->ID_Usuario));
+    // bind values
+    $stmt->bindParam(":US_Nombres", $this->US_Nombres);
+    $stmt->bindParam(":US_Apellidos", $this->US_Apellidos);
+    $stmt->bindParam(":US_Direccion", $this->US_Direccion);
+    $stmt->bindParam(":US_Fecha_Nacimiento", $this->US_Fecha_Nacimiento);
+    $stmt->bindParam(":US_Nacionalidad", $this->US_Nacionalidad);
+    $stmt->bindParam(":US_Telefono", $this->US_Telefono);
+    $stmt->bindParam(":US_Email", $this->US_Email);
+    $stmt->bindParam(":ID_Usuario", $this->ID_Usuario);
+    // execute query
+    if($stmt->execute()){
+        return true;
+        
+    }
+    
+    return false;
+}
 }
 ?>
