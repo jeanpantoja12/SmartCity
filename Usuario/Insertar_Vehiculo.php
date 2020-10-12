1114cb8747a3f182c6e16d6e5243ebb846735ebb
Cecili-Rojas <67488180+Cecili-Rojas@users.noreply.github.com>
2020-10-11 19:53:57 -0500

    Add files via upload

--- Usuario/Insertar_Vehiculo.php
+++ Usuario/Insertar_Vehiculo.php
@@ -10,7 +10,7 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 include_once '../config/database.php';
   
 // instantiate product object
-include_once '../objects/usuario.php';
+include_once '../objects/Vehiculo.php';
   
 $database = new Database();
 $db = $database->getConnection();
