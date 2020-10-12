0edd7ecfddf7a37f0c6e33f3e4094e2d7e2c209a
Cecili-Rojas <67488180+Cecili-Rojas@users.noreply.github.com>
2020-10-11 19:47:34 -0500

    Update Insertar_Vehiculo.php

--- Vehiculo/Insertar_Vehiculo.php
+++ Vehiculo/Insertar_Vehiculo.php
@@ -1,20 +1,20 @@
-<?php
+<?PHP
 // required headers
 header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-  
-// get database connection
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Credentials: true");
+header('Content-Type: application/json');
+
+// include database and object files
 include_once '../config/database.php';
-  
-// instantiate product object
-include_once '../objects/usuario.php';
-  
+include_once '../objects/Vehiculo.php';
+
+// get database connection
 $database = new Database();
 $db = $database->getConnection();
   
+// prepare product object
 $vehiculo = new Vehiculo($db);
   
 // get posted data
