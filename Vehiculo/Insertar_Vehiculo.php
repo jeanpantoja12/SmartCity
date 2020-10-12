2fc7b2b1cf76f50f008b8499891f616d0d523bcf
Cecili-Rojas <67488180+Cecili-Rojas@users.noreply.github.com>
2020-10-11 19:56:34 -0500

    Add files via upload

--- Vehiculo/Insertar_Vehiculo.php
+++ Vehiculo/Insertar_Vehiculo.php
@@ -1,22 +1,24 @@
-<?PHP
+<?php
 // required headers
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: access");
-header("Access-Control-Allow-Methods: GET");
-header("Access-Control-Allow-Credentials: true");
-header('Content-Type: application/json');
-
-// include database and object files
-include_once '../config/database.php';
-include_once '../objects/Vehiculo.php';
-
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+  
 // get database connection
+include_once '../config/database.php';
+  
+// instantiate product object
+include_once '../objects/Vehiculo.php';
+  
 $database = new Database();
 $db = $database->getConnection();
   
-// prepare product object
 $vehiculo = new Vehiculo($db);
-
+  
+// get posted data
+$data = json_decode(file_get_contents("php://input"));
   
 // make sure data is not empty
 if( 
