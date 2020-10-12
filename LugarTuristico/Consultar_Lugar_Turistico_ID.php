2c86e1dc013942382d170a25967a1ab589e53bb8
Jean Pantoja <48846207+jeanpantoja12@users.noreply.github.com>
2020-10-11 21:03:34 -0500

    Add files via upload

---
+++ LugarTuristico/Consultar_Lugar_Turistico_ID.php
@@ -0,0 +1,54 @@
+<?PHP
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Credentials: true");
+header('Content-Type: application/json');
+
+// include database and object files
+include_once '../config/database.php';
+include_once '../objects/Lugar_Turistico.php';
+
+// get database connection
+$database = new Database();
+$db = $database->getConnection();
+  
+// prepare product object
+$lugar = new Lugarturistico($db);
+// set ID property of record to read
+$lugar->ID_Lugar_Turistico = isset($_GET['ID_Lugar_Turistico']) ? $_GET['ID_Lugar_Turistico'] : die();
+  
+// read the details of product to be edited
+$lugar->Consulta();
+  
+if($lugar->ID_Lugar_Turistico!=null){
+    // create array
+    $lugar_arr = array(
+        "ID_Lugar_Turistico" =>  $lugar->ID_Lugar_Turistico,
+        "LT_Nombre" => $lugar->LT_Nombre,
+        "LT_Descripcion" => $lugar->LT_Descripcion,
+        "LT_URL_Map" => $lugar->LT_URL_Map,
+        "ID_Distrito" => $lugar->ID_Distrito,
+        "Distrito" => $lugar->Distrito,
+        "LT_Hora_Inicio" => $lugar->LT_Hora_Inicio,
+        "LT_Hora_Fin" => $lugar->LT_Hora_Fin
+    );
+  
+    // set response code - 200 OK
+    http_response_code(200);
+  
+    // make it json format
+    echo json_encode($lugar_arr);
+}
+  
+else{
+    // set response code - 404 Not found
+    http_response_code(404);
+  
+    // tell the user product does not exist
+    echo json_encode(array("message" => "Lugar no existe."));
+}
+
+
+?>
---
+++ LugarTuristico/Insert_lugar_turistico.php
@@ -0,0 +1,71 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+  
+// get database connection
+include_once '../config/database.php';
+  
+// instantiate product object
+include_once '../objects/Lugar_Turistico.php';
+  
+$database = new Database();
+$db = $database->getConnection();
+  
+$lugart = new Lugarturistico($db);
+  
+// get posted data
+$data = json_decode(file_get_contents("php://input"));
+
+// make sure data is not empty
+if( 
+    !empty($data->LT_Nombre) &&
+    !empty($data->LT_Descripcion) &&
+    !empty($data->LT_URL_Map) &&
+    !empty($data->ID_Distrito) &&
+    !empty($data->LT_Hora_Inicio) &&
+    !empty($data->LT_Hora_Fin)
+){
+  
+    // set product property values
+    $lugart->LT_Nombre = $data->LT_Nombre;
+    $lugart->LT_Descripcion = $data->LT_Descripcion;
+    $lugart->LT_URL_Map = $data->LT_URL_Map;
+    $lugart->ID_Distrito = $data->ID_Distrito;
+    $lugart->LT_Hora_Inicio = $data->LT_Hora_Inicio;
+    $lugart->LT_Hora_Fin = $data->LT_Hora_Fin;
+  
+    // create the product
+    if($lugart->create()){
+  
+        // set response code - 201 created
+        http_response_code(201);
+  
+        // tell the lugart
+        echo json_encode(array("message" => "Lugar turistico creado correctamente."));
+    }
+  
+    // if unable to create the product, tell the lugart
+    else{
+  
+        // set response code - 503 service unavailable
+        http_response_code(503);
+  
+        // tell the lugart
+        echo json_encode(array("message" => "Error en la solicitud"));
+    }
+}
+  
+// tell the lugart data is incomplete
+else{
+  
+    // set response code - 400 bad request
+    http_response_code(400);
+  
+    // tell the lugart
+    echo json_encode(array("message" => "Error, datos incompletos"));
+}
+?>
