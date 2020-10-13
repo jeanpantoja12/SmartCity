b9854e6df43967052ad1b02296c76fd492cea91a
Jean Pantoja <48846207+jeanpantoja12@users.noreply.github.com>
2020-10-12 19:20:08 -0500

    Add files via upload

---
+++ ComentarioObra/Listar_Comentario.php
@@ -0,0 +1,73 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+  
+// get database connection
+include_once '../config/database.php';
+  
+// instantiate product object
+include_once '../objects/Comentario_Obra.php';
+  
+$database = new Database();
+$db = $database->getConnection();
+  
+$comment = new ComentarioObra($db);
+  
+// get posted data
+$data = json_decode(file_get_contents("php://input"));
+
+// make sure data is not empty
+$stmt = $comment->read();
+$num = $stmt->rowCount();
+  
+// check if more than 0 record found
+if($num>0){
+  
+    // products array
+    $comment_arr=array();
+    $comment_arr["records"]=array();
+  
+    // retrieve our table contents
+    // fetch() is faster than fetchAll()
+    // http://stackoverflow.com/questions/2770630/pdofetchall-vs-pdofetch-in-a-loop
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        // extract row
+        // this will make $row['name'] to
+        // just $name only
+        extract($row);
+  
+        $comment_item=array(
+            "ID_Comentario" =>  $ID_Comentario,
+            "ID_Obra" => $ID_Obra,
+            "Obra" => $Obra,
+            "COM_Obra" => $COM_Obra,
+            "COM_Calificacion" => $COM_Calificacion,
+            "ID_Usuario" => $ID_Usuario,
+            "Nombre_Usuario" => $Nombre_Usuario
+        );
+  
+        array_push($comment_arr["records"], $comment_item);
+    }
+  
+    // set response code - 200 OK
+    http_response_code(200);
+  
+    // show sites data in json format
+    echo json_encode($comment_arr);
+}
+  
+else{
+  
+    // set response code - 404 Not found
+    http_response_code(404);
+  
+    // tell the user no products found
+    echo json_encode(
+        array("message" => "Comentarios no encontrados.")
+    );
+}
+?>
