4c54f1be4367b4496ca49a3bc7ccc3d13d7222f5
Cecili-Rojas <67488180+Cecili-Rojas@users.noreply.github.com>
2020-10-13 20:52:21 -0500

    Add files via upload

--- Usuario/Consulta_Usuario_Email.php
+++ Usuario/Consulta_Usuario_Email.php
@@ -1,54 +1,70 @@
-<?PHP
-// required headers
+<?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: access");
 header("Access-Control-Allow-Methods: GET");
 header("Access-Control-Allow-Credentials: true");
 header('Content-Type: application/json');
-
+  
+  
 // include database and object files
 include_once '../config/database.php';
 include_once '../objects/usuario.php';
-  
-// get database connection
+
 $database = new Database();
 $db = $database->getConnection();
-  
-// prepare product object
+ 
+// prepare user object
 $user = new Usuario($db);
-// set ID property of record to read
-$user->US_Email = isset($_GET['US_Email']) ? $_GET['US_Email'] : die();
-  
+
+$data = file_get_contents('php://input');
+$json = json_decode($data);
+
+ 
 // read the details of product to be edited
-$user->ConsultaEmail();
+$US_Email = $json->{'US_Email'};
   
-if($user->US_Email!=null){
+if(!empty($US_Email))
+{
+    $user->US_Email = $US_Email;
+    $stmt = $user->ConsultaEmail();
     // create array
-    $user_arr = array(
-        "ID_Usuario" =>  $user->ID_Usuario,
-        "US_Nombres" => $user->US_Nombres,
-        "US_Apellidos" => $user->US_Apellidos,
-        "US_Direccion" => $user->US_Direccion,
-        "US_Fecha_Nacimiento" => $user->US_Fecha_Nacimiento,
-        "US_Nacionalidad" => $user->US_Nacionalidad,
-        "US_Telefono" => $user->US_Telefono,
-        "US_Email" => $user->US_Email
+   
+    if($stmt->rowCount() > 0){  
+     $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        // create array
+        $user_arr=array(
+            "status" => true,
+            "message" => "Consulta Satisfactoria!",
+            "ID_Usuario" => $row['US_Nombres'],
+            "US_Nombres" => $row['US_Nombres'],
+            "US_Apellidos" => $row['US_Apellidos'],
+            "US_Direccion" => $row['US_Direccion'],
+            "US_Fecha_Nacimiento" => $row['US_Fecha_Nacimiento'],
+            "US_Nacionalidad" => $row['US_Nacionalidad'],
+            "US_Telefono" => $row['US_Telefono'],
+            "US_Email" => $row['US_Email']
     );
   
     // set response code - 200 OK
     http_response_code(200);
-  
-    // make it json format
-    echo json_encode($user_arr);
-}
-  
-else{
-    // set response code - 404 Not found
-    http_response_code(404);
-  
-    // tell the user product does not exist
-    echo json_encode(array("message" => "Usuario no existe."));
+  }else{
+        $user_arr=array(
+            "status" => false,
+            "message" => "Usuario Incorrecto",
+        );
+        http_response_code(404);
+        
+    }
 }
 
+else
+{
+    $user_arr=array(
+            "status" => false,
+            "message" => "Datos Incompletos",
+        );
+    http_response_code(500);
+}
+echo json_encode($user_arr);
 
 ?>
