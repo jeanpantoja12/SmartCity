e61762d6785fc583f58456227967fb7e4ae2c91b
Jean Pantoja <48846207+jeanpantoja12@users.noreply.github.com>
2020-10-18 11:40:00 -0500

    Add files via upload

--- Conductor/Insertar_Conductor.php
+++ Conductor/Insertar_Conductor.php
@@ -7,22 +7,21 @@ header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
   
 // get database connection
-// include database and object files
 include_once '../config/database.php';
+  
+// instantiate product object
 include_once '../objects/Conductor.php';
-
-// get database connection
+  
 $database = new Database();
 $db = $database->getConnection();
   
-$conductor = new Conductor($db);
+$comment = new ComentarioObra($db);
   
 // get posted data
 $data = json_decode(file_get_contents("php://input"));
   
 // make sure data is not empty
 if( 
-  
     !empty($data->CON_Nombre) &&
     !empty($data->CON_Apellidos) &&
     !empty($data->CON_Telefono) &&
@@ -34,11 +33,12 @@ if(
     !empty($data->CON_Longitud) &&
     !empty($data->CON_Status) &&
     !empty($data->CON_FCM) &&
-    !empty($data->CON_Fotografia_Licencia)
- 
- 
+    !empty($data->CON_Fotografia_Licencia) &&
+    !empty($data->CON_Email) &&
+    !empty($data->CON_Contrasena)
 ){
   
+    // set product property values
     $conductor->CON_Nombre = $data->CON_Nombre;
     $conductor->CON_Apellidos = $data->CON_Apellidos;
     $conductor->CON_Telefono = $data->CON_Telefono;
@@ -51,35 +51,36 @@ if(
     $conductor->CON_Status = $data->CON_Status;
     $conductor->CON_FCM = $data->CON_FCM;
     $conductor->CON_Fotografia_Licencia = $data->CON_Fotografia_Licencia;
-
-
+    $conductor->CON_Email = $data->CON_Email;
+    $conductor->CON_Contrasena = $data->CON_Contrasena;
+    // create the product
     if($conductor->create()){
   
-       
+        // set response code - 201 created
         http_response_code(201);
   
-
+        // tell the obra
         echo json_encode(array("message" => "Conductor creado correctamente."));
     }
   
-    // if unable to create the product, tell the user
+    // if unable to create the product, tell the obra
     else{
   
         // set response code - 503 service unavailable
         http_response_code(503);
   
-        // tell the user
+        // tell the obra
         echo json_encode(array("message" => "Error en la solicitud"));
     }
 }
   
-// tell the user data is incomplete
+// tell the obra data is incomplete
 else{
   
     // set response code - 400 bad request
     http_response_code(400);
   
-    // tell the user
+    // tell the obra
     echo json_encode(array("message" => "Error, datos incompletos"));
 }
 ?>
