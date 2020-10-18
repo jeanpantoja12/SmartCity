1fd82b0252ddea6eb046229c558258931b080d20
Cecili-Rojas <67488180+Cecili-Rojas@users.noreply.github.com>
2020-10-18 12:59:37 -0500

    Update Parque.php

--- objects/Parque.php
+++ objects/Parque.php
@@ -13,9 +13,7 @@ class Parques{
     public $ID_Distrito;
     public $Distrito;
     public $PQ_Direccion;
-    public $PQ_Nivel_humedad;
-    public $PQ_Nivel_Radiacion;
-    public $PQ_Nivel_Ruido;
+
 
 
     // constructor with $db as database connection
@@ -27,7 +25,7 @@ class Parques{
     // query to insert record
     $query = "INSERT INTO
                 " . $this->table_name . "
-            SET PQ_Nombre=:PQ_Nombre, PQ_Descripcion=:PQ_Descripcion, ID_Distrito=:ID_Distrito, PQ_Direccion=:PQ_Direccion, PQ_Nivel_humedad=:PQ_Nivel_humedad, PQ_Nivel_Radiacion=:PQ_Nivel_Radiacion, PQ_Nivel_Ruido=:PQ_Nivel_Ruido";
+            SET PQ_Nombre=:PQ_Nombre, PQ_Descripcion=:PQ_Descripcion, ID_Distrito=:ID_Distrito, PQ_Direccion=:PQ_Direccion";
 
     // prepare query
     $stmt = $this->conn->prepare($query);
@@ -37,18 +35,14 @@ class Parques{
     $this->PQ_Descripcion=htmlspecialchars(strip_tags($this->PQ_Descripcion));
     $this->ID_Distrito=htmlspecialchars(strip_tags($this->ID_Distrito));
     $this->PQ_Direccion=htmlspecialchars(strip_tags($this->PQ_Direccion));
-    $this->PQ_Nivel_humedad=htmlspecialchars(strip_tags($this->PQ_Nivel_humedad));
-    $this->PQ_Nivel_Radiacion=htmlspecialchars(strip_tags($this->PQ_Nivel_Radiacion));
-    $this->PQ_Nivel_Ruido=htmlspecialchars(strip_tags($this->PQ_Nivel_Ruido));
+
     // bind values
 
     $stmt->bindParam(":PQ_Nombre", $this->PQ_Nombre);
     $stmt->bindParam(":PQ_Descripcion", $this->PQ_Descripcion);
     $stmt->bindParam(":ID_Distrito", $this->ID_Distrito);
     $stmt->bindParam(":PQ_Direccion", $this->PQ_Direccion);
-    $stmt->bindParam(":PQ_Nivel_humedad", $this->PQ_Nivel_humedad);
-    $stmt->bindParam(":PQ_Nivel_Radiacion", $this->PQ_Nivel_Radiacion);
-    $stmt->bindParam(":PQ_Nivel_Ruido", $this->PQ_Nivel_Ruido);
+
    
 
     // execute query
@@ -62,7 +56,7 @@ class Parques{
 function Consulta(){
         // query to read single record
     $query = "SELECT
-                p.ID_Parque, p.PQ_Nombre, p.PQ_Descripcion, p.ID_Distrito,d.DIS_Nombre as Distrito,p.PQ_Direccion,p.PQ_Nivel_humedad, p.PQ_Nivel_Ruido,p.PQ_Nivel_Radiacion
+                p.ID_Parque, p.PQ_Nombre, p.PQ_Descripcion, p.ID_Distrito,d.DIS_Nombre as Distrito,p.PQ_Direccion
             FROM
                 " . $this->table_name . " p
                 LEFT JOIN
@@ -91,16 +85,13 @@ function Consulta(){
     $this->ID_Distrito = $row['ID_Distrito'];
     $this->Distrito = $row['Distrito'];
     $this->PQ_Direccion = $row['PQ_Direccion'];
-    $this->PQ_Nivel_humedad = $row['PQ_Nivel_humedad'];
-    $this->PQ_Nivel_Ruido = $row['PQ_Nivel_Ruido'];
-    $this->PQ_Nivel_Radiacion = $row['PQ_Nivel_Radiacion'];
     }
   
   function read(){
   
     // select all query
     $query = "SELECT
-                p.ID_Parque, p.PQ_Nombre, p.PQ_Descripcion, p.ID_Distrito,d.DIS_Nombre as Distrito,p.PQ_Direccion, p.PQ_Nivel_humedad, p.PQ_Nivel_Radiacion, p.PQ_Nivel_Ruido
+                p.ID_Parque, p.PQ_Nombre, p.PQ_Descripcion, p.ID_Distrito,d.DIS_Nombre as Distrito,p.PQ_Direccion
             FROM
                 " . $this->table_name . " p
                 LEFT JOIN
