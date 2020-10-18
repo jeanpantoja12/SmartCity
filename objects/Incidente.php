65802ea76777c9795d210f5698d0c1de514e55d6
giancohg <67488267+giancohg@users.noreply.github.com>
2020-10-18 17:56:04 -0500

    Update Incidente.php

--- objects/Incidente.php
+++ objects/Incidente.php
@@ -101,8 +101,7 @@ class Incidente{
                     Tbl_Usuario u
                         ON i.ID_Usuario = u.ID_Usuario
             WHERE
-                i.ID_Vehiculo = ?
-            ";
+                i.ID_Vehiculo = ?";
                 // prepare query statement
     $stmt = $this->conn->prepare( $query );
   
