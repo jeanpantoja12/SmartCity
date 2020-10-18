7c5e5915651fb03752eebe81c4aebfe1a41e72f8
Jean Pantoja <48846207+jeanpantoja12@users.noreply.github.com>
2020-10-18 12:23:28 -0500

    Add files via upload

---
+++ Conductor/login.php
@@ -0,0 +1,59 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Credentials: true");
+header('Content-Type: application/json');
+  
+  
+// include database and object files
+include_once '../config/database.php';
+include_once '../objects/usuario.php';
+ $database = new Database();
+$db = $database->getConnection();
+ 
+// prepare user object
+$user = new Conductor($db);
+
+$data = file_get_contents('php://input');
+$json = json_decode($data);
+
+$email = $json->{'CON_Email'};
+$password = $json->{'CON_Contrasena'};
+
+if(!empty($email) and !empty($password))
+{
+    $user->CON_Email = $email;
+    $user->CON_ContrasenaN = $password;
+    $stmt = $user->login();
+    if($stmt->rowCount() > 0){
+        // get retrieved row
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        // create array
+        $user_arr=array(
+            "status" => true,
+            "message" => "Login Satisfactorio!",
+            "ID_Conductor" => $row['ID_Conductor'],
+            "CON_Email" => $row['CON_Email']
+        );
+        http_response_code(200);
+    }
+    else{
+        $user_arr=array(
+            "status" => false,
+            "message" => "Conductor Incorrecto",
+        );
+        http_response_code(404);
+        
+    }
+}
+else
+{
+    $user_arr=array(
+            "status" => false,
+            "message" => "Datos Incompletos",
+        );
+    http_response_code(500);
+}
+echo json_encode($user_arr);
+?>
