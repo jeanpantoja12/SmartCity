605db32d6fe572456ab1103cf09d545f8ff04b54
giancohg <67488267+giancohg@users.noreply.github.com>
2020-10-18 19:29:06 -0500

    Update Incidente.php

--- objects/Incidente.php
+++ objects/Incidente.php
@@ -101,26 +101,23 @@ class Incidente{
                     Tbl_Usuario u
                         ON i.ID_Usuario = u.ID_Usuario
             WHERE
-                i.ID_Vehiculo = ?";
+                i.ID_Vehiculo = ?
+            ORDER BY
+                i.ind_Fecha_Incidente DESC";
+            
                 // prepare query statement
     $stmt = $this->conn->prepare( $query );
   
     // bind id of product to be updated
-    $stmt->bindParam(1, $this->ID_Vehiculo);
+    $keywords=htmlspecialchars(strip_tags($keywords));
+    $keywords = "%{$keywords}%";
+    $stmt->bindParam(1, $this->$keywords);
   
     // execute query
     $stmt->execute();
   
     // get retrieved row
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
-  
-    // set values to object properties
-    $this->ID_Incidente = $row['ID_Incidente'];
-    $this->ID_Vehiculo = $row['ID_Vehiculo'];
-    $this->ID_Usuario = $row['ID_Usuario'];
-    $this->ind_Descripcion = $row['ind_Descripcion'];
-    $this->ind_Fotografia = $row['ind_Fotografia'];
-    $this->ind_Fecha_Incidente = $row['ind_Fecha_Incidente'];
-    }
+    return $stmt
 }
+
 ?>
