cc76cca75331fc6314da5b0027b8ddcdeac80f53
Jean Pantoja <48846207+jeanpantoja12@users.noreply.github.com>
2020-10-19 18:57:34 -0500

    Update Consultar_Incidente_fecha.php

--- Incidente/Consultar_Incidente_fecha.php
+++ Incidente/Consultar_Incidente_fecha.php
@@ -1,10 +1,7 @@
 <?PHP
 // required headers
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: access");
-header("Access-Control-Allow-Methods: GET");
-header("Access-Control-Allow-Credentials: true");
-header('Content-Type: application/json');
+header("Content-Type: application/json; charset=UTF-8");
 
 include_once '../config/core.php';
 include_once '../config/database.php';
