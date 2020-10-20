48ad12b13bfafb0a7601f499e1ecff221df9716d
Cecili-Rojas <67488180+Cecili-Rojas@users.noreply.github.com>
2020-10-19 19:07:41 -0500

    Add files via upload

---
+++ objects/Video_LugarTuristico.php
@@ -0,0 +1,23 @@
+
+<?php
+class Video_LT{
+  
+    // database connection and table name
+    private $conn;
+    private $table_name = "Tbl_Video_LugarTuristico";
+
+      // object properties
+    public $ID_Video_LT;
+    public $ID_Lugar_Turistico;
+    public $VL_Descripcion;
+    public $L_URL;
+
+    // constructor with $db as database connection
+    public function __construct($db){
+        $this->conn = $db;
+    }
+
+  
+
+}
+?>
